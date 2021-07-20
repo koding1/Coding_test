16671147ed0f6f9f9c9cfe1dd361e7f310381e11
koding1 <48898974+koding1@users.noreply.github.com>
2021-07-20 21:59:19 +0900

    Create 43165.py

---
+++ programmers/43165(타겟넘버)-bfs,dfs/43165.py
@@ -0,0 +1,22 @@
+# bfs - queue 사용
+from collections import deque
+    
+def bfs_queue(numbers, target):
+    answer = 0
+    under = len(numbers)
+    queue = deque()
+
+    queue.append([numbers[0], 0])
+    queue.append([(-1 * numbers[0]), 0])
+    
+    while queue:
+        v, d = queue.popleft()
+
+        # 모든 숫자를 다 사용한 깊이 and 값 == 타겟 일 때
+        if d == under-1 and v == target:
+            answer += 1
+        
+        if d+1 < under:
+            queue.append([v + numbers[d+1], d+1])
+            queue.append([v + (-1 * numbers[d+1]), d+1])
+    return answer
