f124e9ec0d4d75ff2baf64a71244a0e58b3b9fe7
koding1 <48898974+koding1@users.noreply.github.com>
2021-07-22 22:53:00 +0900

    Update 43162.py

--- programmers/43162(네트워크)-BFS,DFS/43162.py
+++ programmers/43162(네트워크)-BFS,DFS/43162.py
@@ -30,4 +30,42 @@ def solution(n, computers):
             
     return answer
   
-  ##################################################################################################
+##################################################################################################
+
+# DFS - recursion
+answer = 0
+def DFS(n, computers, num, visited):
+    global answer
+    
+    visited[num] = True
+    
+    check = visited.count(False)
+    if check == 0:
+        return
+    # 아직 연결 확인하지 못한 컴퓨터가 여러 대(2대 이상)가 남았다면
+    else:
+        tmp = 0
+        corrent = computers[num]
+    
+        for i in range(0, len(corrent)):
+            if (corrent[i] == 1) and (not visited[i]):
+                tmp = 1
+                DFS(n, computers, i, visited)
+
+    return
+
+def solution(n, computers):
+    global answer
+    
+    visited = [False] * n
+    
+    # 이 while 문 추가하는게 아이디어 떠올리기가 어려웠어요.
+    # 보통 DFS 함수를 재귀 밖에서 호출하는 횟수가 1번이였기 때문에..
+    while visited.count(False) != 0:
+            answer += 1
+            # 가장 앞에 있는 False의 인덱스
+            i = visited.index(False)
+            DFS(n, computers, i, visited)
+    
+    
+    return answer
