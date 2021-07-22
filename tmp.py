ac9f92cb7b4a1b0cf7c957b23c1dc77124ab0501
koding1 <48898974+koding1@users.noreply.github.com>
2021-07-22 23:43:44 +0900

    Create tmp.py

---
+++ tmp.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def solution(tickets):
+    answer = []
+    
+    # n + 1 == 전체 도시의 수
+    n = len(tickets)
+    
+    q = deque()
+    
+    q.append(['starting','ICN', tickets, ['ICN']])
+    
+    while q:
+        # end 는 현재 내 위치라고 생각해도 무방 (이미 도착했다고 생각)
+        start, end, ticket, visited = q.popleft()
+        
+        # 사용 할 수 있는 티켓이 없다면
+        if not ticket:
+            return visited
+        
+        can_list = []
+        for i in range(0, len(ticket)):
+            # 현재 내 위치(end)에서 탈 수 있는 비행기 조회
+            if (ticket[i][0] == end):
+                can_list.append(ticket[i][1])
+                
+        print(can_list)
+        can_list.sort()
+        tmp = can_list[0]
+        del_index = ticket.index([end, tmp])
+            
+        q.append([end, tmp, (ticket[:del_index]+ticket[del_index+1:]), (visited + [tmp])])
+                
+    return visited
+  # https://programmers.co.kr/learn/courses/30/lessons/43164
