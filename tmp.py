3b9b6fa88fe26bcb0c71c8097e01d64e70f3c6e2
koding1 <48898974+koding1@users.noreply.github.com>
2021-07-24 23:24:41 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,37 +1,57 @@
-from collections import deque
+from datetime import datetime, timedelta
 
-def solution(tickets):
-    answer = []
+# start ~ start + 1 사이에 tmp가 해당하는지 검사
+def check(s, tmp):
     
-    # n + 1 == 전체 도시의 수
-    n = len(tickets)
-    
-    q = deque()
-    
-    q.append(['starting','ICN', tickets, ['ICN']])
-    
-    while q:
-        # end 는 현재 내 위치라고 생각해도 무방 (이미 도착했다고 생각)
-        start, end, ticket, visited = q.popleft()
+    e = s + timedelta(seconds=1)
+    # print("-------------------")
+    # print(s)
+    # print(e)
+    # print(tmp[0])
+    # print(tmp[1])
+    # print("-------------------")
+    # 왼쪽 선에 걸친 경우
+    if s >= tmp[0] and s <= tmp[1]:
+        print("T1")
+        return True
+    # 오른쪽 선에 걸친 경우
+    if e >= tmp[0] and e <= tmp[1]:
+        print("T2")
+        return True
+    # 박스 안에 있는 경우
+    if s <= tmp[0] and e >= tmp[1]:
+        print("T3")
+        return True
         
-        # 사용 할 수 있는 티켓이 없다면
-        if not ticket:
-            return visited
-        
-        can_list = []
-        for i in range(0, len(ticket)):
-            # 현재 내 위치(end)에서 탈 수 있는 비행기 조회
-            if (ticket[i][0] == end):
-                can_list.append(ticket[i][1])
-                
-        # can_list 에 원소가 있다면
-        if can_list:
-            can_list.sort()
-            tmp = can_list[0]
-            del_index = ticket.index([end, tmp])
-            q.append([end, tmp, (ticket[:del_index]+ticket[del_index+1:]), (visited + [tmp])])
-                
-    return visited
+    print("F")
+    return False
+def solution(lines):
+    answer = 0
     
-print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
-# https://programmers.co.kr/learn/courses/30/lessons/43164
+    data = []
+    
+    # day 날리고 시작시간 구해서 시작시간으로 재정렬
+    for i in range(len(lines)):
+        
+        dateFormatter = "%Y-%m-%d %H:%M:%S.%f"
+        end = datetime.strptime(lines[i][:23], dateFormatter)
+        tmp = float(lines[i][24:-1])
+        start = end - timedelta(seconds=(tmp - 0.001))
+        
+        data.append([start, end, tmp])
+
+    
+    for i in range(len(data)):
+        max_tmp = 0
+        
+        for j in range(len(data)):
+            # 시작시간과 검사할 데이터를 전달
+            if check(data[i][0], data[j]):
+                max_tmp += 1
+        
+        answer = max(answer, max_tmp)
+    return answer
+    
+print(solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"]))
+
+# https://programmers.co.kr/learn/courses/30/lessons/17676?language=python3
