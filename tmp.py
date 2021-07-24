652179ea30e731a2034989c1b3ec8201d954bce4
koding1 <48898974+koding1@users.noreply.github.com>
2021-07-24 23:26:14 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -55,3 +55,5 @@ def solution(lines):
 print(solution(["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"]))
 
 # https://programmers.co.kr/learn/courses/30/lessons/17676?language=python3
+# 02.001 ~ 04.002
+# 05.001 ~ 07.000
