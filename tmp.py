13380bc357fc25f8ff1ebb79eb46798f3f8cf5c5
koding1 <48898974+koding1@users.noreply.github.com>
2021-08-02 23:32:53 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,40 +1,25 @@
 from collections import deque
 
-def solution(n, t, m, timetable):
+def solution(cacheSize, cities):
     answer = 0
-    timetable = sorted(timetable)
-    # timetable을 분으로 변환한 큐
-    tq = deque()
     
-    # 분으로 단위 통일
-    for i in range(len(timetable)):
-        hh, mm = [int(j) for j in timetable[i].split(':')]
-        mm = (hh * 60) + mm
-        tq.append(mm)
+    cities = [q.lower() for q in cities]
     
-    # 오름차순 정렬
-    # tq = sorted(tq)
-    # print(tq)
+    q = deque()
     
-    # 09:00 를 분으로 변환
-    start = 540
-    for i in range(n):
-        cnt = 0
-        while tq and cnt < m:
-            # 탈 수 있다면
-            if tq[0] <= start:
-                cnt += 1
-                # 마지막 남은 자리라면
-                if (cnt == m) and (i == n - 1):
-                    tmp = (tq.popleft() - 1)
-                    return  str(tmp // 60).rjust(2, '0') + ':' + str(tmp % 60).rjust(2, '0')
+    for city in cities:
+        if city in q:
+            answer += 1
+            if len(q) >= cacheSize:
+                if q[0] != city:
+                    q.remove(city)
                 else:
-                    print(cnt, m, i, t)
-                    tq.popleft()
-            else:
-                break
-        if (i == n - 1):
-            tmp = 540 + (i * t)
-            return str(tmp // 60).rjust(2, '0') + ':' + str(tmp % 60).rjust(2, '0')
-        start += t
+                    q.popleft()
+                
+        else:
+            answer += 5
+            if q and len(q) >= cacheSize:
+                q.popleft()
+        q.append(city)
+        
     return answer
