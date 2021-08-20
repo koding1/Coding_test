abfe5db30267ce69f7909ee976629a5b5b9c8dfd
koding1 <48898974+koding1@users.noreply.github.com>
2021-08-20 22:02:03 +0900

    Update 42895.py

--- programmers/42895(N으로표현)-동적계획법/42895.py
+++ programmers/42895(N으로표현)-동적계획법/42895.py
@@ -21,40 +21,51 @@ def solution(N, number):
                         tmp2.append(k // l)
                     tmp2.append(k * l)
                     
-                    if number in tmp2:
-                        return i
+            if number in tmp2:
+                return i
             tmp = tmp + tmp2
-        nums[i] = tmp
+        if i < 8:
+            nums[i] = tmp
+        else:
+            return -1
     
     return -1
 
 print(solution(2, 11))
 
 
-# 속도 차이 
+# 같은 로직이지만 set를 사용했을 때 훨씬 빠르다 (속도 차이) 
 
 def solution(N, number):
-    answer = -1
-    DP = []
+
+    # N을 i번 만큼 사용 했을 때 만들 수 있는 숫자 리스트
+    nums = [[] for i in range(8)]
 
     for i in range(1, 9):
-        numbers = set()
-        numbers.add( int(str(N) * i) )
-        
-        for j in range(0, i-1):
-            for x in DP[j]:
-                for y in DP[-j-1]:
-                    numbers.add(x + y)
-                    numbers.add(x - y)
-                    numbers.add(x * y)
+        tmp = set()
+
+        n1 = int(str(N) * i)
+        tmp.add(n1)
+        if n1 == number:
+            return i
+
+        for j in range(1, i):
+            tmp2 = set()
+            for k in nums[j]:
+                for l in nums[i - j]:
+                    tmp2.add(k + l)
+                    tmp2.add(k - l)
+                    if l != 0:
+                        tmp2.add(k // l)
+                    tmp2.add(k * l)
                     
-                    if y != 0:
-                        numbers.add(x // y)
+            if number in tmp2:
+                return i
+            tmp = tmp | tmp2
 
-        if number in numbers:
-            answer = i
-            break
-        
-        DP.append(numbers)
+        if i < 8:
+            nums[i] = tmp
+        else:
+            return -1
 
-    return answer
+    return -1
