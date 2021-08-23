def71c5bbad541e90a675e282d30093c26ad045e
koding1 <48898974+koding1@users.noreply.github.com>
2021-08-23 23:35:21 +0900

    Update 13305.py

--- BJ/Greedy/13305.py
+++ BJ/Greedy/13305.py
@@ -16,19 +16,22 @@ while current_index + 1 != N:
     # 현재 위치한 도시의 기름 가격
     now_cost = cost[current_index]
     
-    # now_cost 보다 싼 도시까지의 필요 이동거리 (다음 도시까지는 이동했다고 초기화)
-    need_d = distance[current_index]
+    # now_cost 보다 싼 도시까지의 필요 이동거리 (0)
+    need_d = 0
     
     # 내 도시보다 싼 도시를 찾을 때 까지
     next_index = current_index + 1
-    while now_cost < cost[next_index]:
-        if next_index + 1 < N:
-            next_index += 1
+    while True:
         need_d += distance[next_index-1]
+        if (now_cost > cost[next_index]) or (next_index + 1 >= N):
+            break
+        next_index += 1
+        
+        
     
     # 이동하기 위한 기름 충전
     sum_cost = sum_cost + (need_d * now_cost)
-    
+    #print(need_d,"만큼 이동 하기 위해", now_cost, "원으로 삽니다", need_d*now_cost)
     # 이동
     current_index = next_index
     
