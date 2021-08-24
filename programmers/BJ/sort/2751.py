14386b18a96c88810c63d8b3653fe5e6718f35e7
koding1 <48898974+koding1@users.noreply.github.com>
2021-08-24 23:01:47 +0900

    Update 2751.py

--- programmers/BJ/sort/2751.py
+++ programmers/BJ/sort/2751.py
@@ -5,8 +5,8 @@ for i in range(N):
     numbers.append(int(input()))
 
 # 1. sorted 함수 사용
-for i in sorted(numbers):
-    print(i)
+# for i in sorted(numbers):
+#     print(i)
 
 # 2. merge sort (병합 정렬)
 def merge_sort(array):
@@ -33,13 +33,19 @@ def merge_sort(array):
             k += 1
             j += 1
         
-        if i >= len(left):
-            array[k] = right[j]
+        if i >= len(left): # left 는 모두 삽입
+            while j < len(right): # 남은 right 삽입
+                array[k] = right[j]
+                k += 1
+                j += 1
             break
-        if j >= len(right):
-            array[k] = left[i]
+        if j >= len(right): # right 는 모두 삽입
+            while i < len(left): # 남은 left 삽입
+                array[k] = left[i]
+                k += 1
+                i += 1
             break
     
-    return tmp
+    return array
 for i in merge_sort(numbers):
     print(i)
